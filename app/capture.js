ca78e5bf17fef91aba43ff49e23e3afcc21cdb78
Martin Leopold <m@martinleopold.com>
2018-02-22 19:05:13 +0100

    Added one more fixme note

--- app/capture.js
+++ app/capture.js
@@ -1,7 +1,7 @@
 /* globals CCapture */
 
 /*
- *  TODO:
+ *  FIXME:
  *  ccapture doesn't work as a module, so we can't loose the dependency in index.html
  *  -> fork / reimplement. only need tar. (webm doesn't work anyway)
  *
@@ -9,6 +9,8 @@
  *
  *  repeated captures with startTime + timeLimit yield different results
  *  (also applies to repeated caputres WITHOUT startTime and/or timeLimit)
+ *
+ *  first recorded frame is wrong (i.e. wrong timimg)
  */
 
 let capturer;
